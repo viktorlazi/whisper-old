48169929ba0715801b7876f57fcf846a53b9f663
Viktorlazi <viktorlazi010@gmail.com>
2021-01-25 13:21:15 +0100

    setup router-dom

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import Chat from './components/Chat'
+import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
   return (
-    <div className="App">
-      test    
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/chat" component={Chat} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </div>
+    </Router>
   );
 }
 
