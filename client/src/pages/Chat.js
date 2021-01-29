9762849b0f3d8c63e98fb518205a01b46dd5a21e
Viktorlazi <viktorlazi010@gmail.com>
2021-01-29 10:09:09 +0100

    testing socketio on react and node successful

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -4,7 +4,8 @@ import ChatBody from './components/ChatBody'
 import Sidebar from './components/Sidebar'
 import io from 'socket.io-client'
 
-const socket = io.connect('http://localhost:4000')
+const socket = io('http://localhost:4000')
+console.log(socket)
 
 export default function Chat() {
   const [addressToken, setAdressToken] = useState(1)
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import * as io from "socket.io" 
 import express from 'express'; 
 import {createServer} from 'http'; 
+import cors from 'cors'
 
 const app = express();  
 const server = createServer(app);  
+app.use(cors())
 const socketio = new io.Server(server);  
 
 socketio.on('connection', (socket)=>{
@@ -14,11 +16,7 @@ server.listen(4000, ()=>{
   console.log('listening on 4000')
 })
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
-io.on('connection', (socket) => {
+socketio.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     socket.broadcast.emit('chat message', msg)
   });
