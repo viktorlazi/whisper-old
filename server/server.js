cdf81cdacf41216cb0124b72826c22e8dd45c5ae
Viktorlazi <viktorlazi010@gmail.com>
2021-01-29 20:27:06 +0100

    mongo setup 3rd time

--- server/server.js
+++ server/server.js
@@ -2,11 +2,22 @@ import * as io from "socket.io"
 import express from 'express'; 
 import {createServer} from 'http'; 
 import cors from 'cors'
+import mongo_password from './mongo_pass'
 
 const app = express();  
-const server = createServer(app);  
+const server = createServer(app); 
 app.use(cors())
 const socketio = new io.Server(server);  
+
+//mongo
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:"+ mongo_password +"@cluster0.lkgyy.mongodb.net/whisper?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 socketio.on('connection', (socket)=>{
   console.log('user connected')
