e03e5f5264c9b51fe5810070fac48106554cbd5a
Viktorlazi <viktorlazi010@gmail.com>
2021-02-10 10:46:37 +0100

    adding welcome chat screen

--- client/src/pages/components/NoChat.js
+++ client/src/pages/components/NoChat.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './noChat.css';
 
 export default function NoChat() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<div class="no_chat">
+				<h1>Welcome to Whisper</h1>
+		</div>
+	)
 }
